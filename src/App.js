bbfbe7201e42b56010ec1b0b4387e2644cb2f1b1
anaharri <anaharringtonb@gmail.com>
2021-08-24 22:44:33 -0300

    switched fetch for axios on App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
 import React, { useState } from 'react'
 import './App.css'
-// import Card from './components/Card.jsx'
 import Cards from './components/Cards.jsx'
 import Nav from './components/Nav.jsx'
-import fetch from 'node-fetch'
-// import data from './data.js'
+import axios from 'axios'
 
 //Paleta de colores: https://coolors.co/e63946-f1faee-a8dadc-457b9d-1d3557
 
 function App() {
   const [cities, setCities] = useState([])
 
-  function onSearch(city) {
+  async function onSearch(city) {
     const apiKey = '4ae2636d8dfbdc3044bede63951a019b'
-    fetch(
+    // fetch(
+    //   `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     if (data.main !== undefined) {
+    //       const newCity = {
+    //         id: data.id,
+    //         name: data.name,
+    //         min: data.main.temp_min,
+    //         max: data.main.temp_max,
+    //         img: data.weather[0].icon,
+    //       }
+    //       if (cities.includes(newCity) === false) {
+    //         setCities((currentCities) => [...currentCities, newCity])
+    //       }
+    //     } else {
+    //       alert('Ciudad no encontrada')
+    //     }
+    //   })
+    let cityInfo = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     )
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.main !== undefined) {
-          const newCity = {
-            id: data.id,
-            name: data.name,
-            min: data.main.temp_min,
-            max: data.main.temp_max,
-            img: data.weather[0].icon,
-          }
-          if (cities.includes(newCity) === false) {
-            setCities((currentCities) => [...currentCities, newCity])
-          }
-        } else {
-          alert('Ciudad no encontrada')
-        }
-      })
+
+    cityInfo = cityInfo.data
+
+    console.log(cityInfo)
+    if (cityInfo.main !== undefined) {
+      const newCity = {
+        id: cityInfo.id,
+        name: cityInfo.name,
+        min: cityInfo.main.temp_min,
+        max: cityInfo.main.temp_max,
+        img: cityInfo.weather[0].icon,
+      }
+      if (cities.includes(newCity) === false) {
+        setCities((currentCities) => [...currentCities, newCity])
+      }
+    } else {
+      alert('Ciudad no encontrada')
+    }
   }
 
   function onClose(id) {
